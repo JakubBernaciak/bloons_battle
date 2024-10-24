ef6d369261d81bedb55ba6c198fcfe4fc2d91447
JakubBernaciak <bernaciak.jm@gmail.com>
2024-10-24 20:23:15 +0200

    Add typing

--- src/game.py
+++ src/game.py
@@ -3,6 +3,6 @@ from player import Player
 
 class Game():
     def __init__(self, p1_name, p2_name):
-        self.p1 = Player(p1_name)
-        self.p2 = Player(p2_name)
+        self.p1: Player = Player(p1_name)
+        self.p2: Player = Player(p2_name)
         self.round = 1
