b1c96b5a7611dfe638f9e1b76fe0014a25a21a07
JakubBernaciak <bernaciak.jm@gmail.com>
2024-10-24 18:26:40 +0200

    Remove redundant comments and add displaying interface

--- src/main.py
+++ src/main.py
@@ -1,27 +1,23 @@
-# Example file showing a basic pygame "game loop"
 import pygame
+from interface import Interface
 
-# pygame setup
+RESOLUTION = (1280,720)
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
 clock = pygame.time.Clock()
 running = True
-
+interface = Interface(resolution=RESOLUTION)
+p1 = 100
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+    interface.display_interface(p1)
+    p1 -= 1
+    if p1 == -1:
+        p1 = 100
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)
 
 pygame.quit()
